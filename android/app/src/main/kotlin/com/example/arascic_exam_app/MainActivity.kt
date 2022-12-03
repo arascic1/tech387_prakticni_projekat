b68506fc77d30cdf0250c22c12cd86d877599816
arascic <arascic1@etf.unsa.ba>
2022-12-04 00:40:53 +0100

    početak

---
+++ android/app/src/main/kotlin/com/example/arascic_exam_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.arascic_exam_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+// AHMED RAŠČIĆ
+// AHMED RAŠČIĆ
+// AHMED RAŠČIĆ
+// AHMED RAŠČIĆ
+// AHMED RAŠČIĆ
+// AHMED RAŠČIĆ
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Ispitna aplikacija',
+
+    );
+  }
+}
